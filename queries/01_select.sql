332f7268bddecb3c181a1f53e977149be53c5a87
Sabin Shrestha <ssabin732@gmail.com>
2025-04-08 23:25:54 +0100

    Create 01_select.sql

---
+++ queries/01_select.sql
@@ -0,0 +1,19 @@
+SQL-Basics-Project/
+│
+├── README.md                 ← Describe your project in detail
+│
+├── dataset/                 
+│   └── dvdrental.tar         ← Upload the sample DB file (optional)
+│
+├── queries/
+│   ├── 01_select.sql
+│   ├── 02_where.sql
+│   ├── 03_order_by.sql
+│   ├── 04_group_by.sql
+│   ├── 05_having.sql
+│   ├── 06_joins.sql
+│   └── 07_case_when.sql
+│
+└── analysis/
+    └── sales_summary_report.sql  ← Your mini project/report based on analysis
+ 
